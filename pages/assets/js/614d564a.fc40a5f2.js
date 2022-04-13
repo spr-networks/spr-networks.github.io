b97061404af5a1a505f5aca55b3a267a599044f6
ltsrad <alex+rad@longterm.io>
2022-04-13 14:54:20 -0700

    bump

---
+++ pages/assets/js/614d564a.fc40a5f2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkspr_docs=self.webpackChunkspr_docs||[]).push([[4950],{7538:function(e){e.exports=JSON.parse('{"permalink":"/pages/blog/tags/spr","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"previousPage":null,"nextPage":null,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
---
+++ pages/assets/js/e9d3736a.4d38f4a3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkspr_docs=self.webpackChunkspr_docs||[]).push([[4572],{5379:function(s){s.exports=JSON.parse('{"allTagsPath":"/pages/blog/tags","slug":"/pages/blog/tags/spr","name":"SPR","count":4,"permalink":"/pages/blog/tags/spr"}')}}]);
